cfe53f657eaaa80332fb3e8af8b6108f9d1b8436
Douglas Pinheiro <email@douglaspinheiro.dev>
2021-02-20 14:25:57 -0300

    install pg, add knex configs

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+require('dotenv').config()
+
+module.exports = {
+    client: 'mysql',
+    connection: {
+      database: process.env.database,
+      user:     process.env.user,
+      password: process.env.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./database/migrations",
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+
+
+};
