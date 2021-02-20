2bb7c74a0710fa511d1783f6fd62ad10d52dbd57
Douglas Pinheiro <email@douglaspinheiro.dev>
2021-02-20 19:43:39 -0300

    Delete encurtador.js

--- routes/encurtador.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const encurtadorController = require('../components/encurtador/controller')
-/* GET users listing. */
-router.get('/',encurtadorController.salvar);
-
-module.exports = router;
