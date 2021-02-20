77fff225e0b078f079b731f92bc5045a68783fa7
Douglas Pinheiro <email@douglaspinheiro.dev>
2021-02-20 19:46:45 -0300

    Update app.js
    Adicionado cors, bodyparser e Tratamento de erros

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
-var encurtadorRouter = require('./routes/encurtador');
 
 var app = express();
 
@@ -13,8 +14,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use('/', indexRouter);
-app.use('/encurtador', encurtadorRouter);
 
+
+app.use(function(err, req, res, next) {
+    console.log(err);
+    if (err.status) {
+        res.status(err.status).send(err)
+    } else {
+
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.send(err.message);
+    }
+})
 module.exports = app;
