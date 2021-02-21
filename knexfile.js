1105837aacabcf4712279ffb4b4dc2a7dc161be2
Douglas Pinheiro <email@douglaspinheiro.dev>
2021-02-21 18:30:59 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,13 @@ require('dotenv').config()
 
 module.exports = {
     client: 'pg',
-    connection: {
-      host: process.env.HOST,
-      database: process.env.DATABASE,
-      user:     process.env.USER,
-      password: process.env.PASSWORD
-    },
-    // connection: process.env.URI,
+    // connection: {
+    //   host: process.env.HOST,
+    //   database: process.env.DATABASE,
+    //   user:     process.env.USER,
+    //   password: process.env.PASSWORD
+    // },
+    connection: process.env.DATABASE_URL,
     searchPath: ['knex', 'public'],
     pool: {
       min: 2,
