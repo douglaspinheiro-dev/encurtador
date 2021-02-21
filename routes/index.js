56c51e3df5ac99bc99efde8f2b8f9c69dd56618c
Douglas Pinheiro <email@douglaspinheiro.dev>
2021-02-21 18:04:25 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const encurtadorController = require('../components/encurtador/controller')
 /* GET users listing. */
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
 router.post('/encurtador',encurtadorController.salvar);
 router.get('/:newUrl',encurtadorController.selecionar);
 
